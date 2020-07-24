f30aa5259f21180430db8a6d3f3feaf38c67d4fa
tommyrharper <tommyrharper@gmail.com>
2020-07-24 13:23:15 +0100

    Add 5th test

--- spec/mixed_fraction_spec.rb
+++ spec/mixed_fraction_spec.rb
@@ -16,4 +16,8 @@ describe 'mixed_fraction' do
   it 'returns -2/3 for -2/3' do
     expect(mixed_fraction('-2/3')).to eq '-2/3'
   end
+
+  it 'returns -2/3 for 4/-6' do
+    expect(mixed_fraction('4/-6')).to eq '-2/3'
+  end
 end
