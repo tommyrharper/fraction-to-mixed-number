431a25487c9615906073db006dfbfbb80c4a69b2
tommyrharper <tommyrharper@gmail.com>
2020-07-24 13:27:12 +0100

    Add test 6 for 10/-7

--- spec/mixed_fraction_spec.rb
+++ spec/mixed_fraction_spec.rb
@@ -20,4 +20,8 @@ describe 'mixed_fraction' do
   it 'returns -2/3 for 4/-6' do
     expect(mixed_fraction('4/-6')).to eq '-2/3'
   end
+
+  it 'returns -1 3/7 for 10/-7' do
+    expect(mixed_fraction('10/-7')).to eq '-1 3/7'
+  end
 end
