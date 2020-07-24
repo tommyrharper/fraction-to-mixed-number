78d64e4b07a4c8813470133fad56b7580230d2e0
tommyrharper <tommyrharper@gmail.com>
2020-07-24 12:39:30 +0100

    Refactor method

--- lib/mixed_fraction.rb
+++ lib/mixed_fraction.rb
@@ -1,16 +1,13 @@
-def mixed_fraction(simple_fraction)
-
-  numerator = simple_fraction.split('/')[0].to_i
-  denominator = simple_fraction.split('/')[1].to_i
-  remainder = numerator % denominator
-
-  if remainder == 0
+def mixed_fraction(fraction)
+  numerator = fraction.split('/')[0].to_i
+  denominator = fraction.split('/')[1].to_i
+  if numerator % denominator == 0
     mixed_fraction = (numerator / denominator).to_s
   else 
-    first_number = (numerator / denominator).floor
-    new_numerator = (numerator - (first_number * denominator)).to_s
-    fraction = Rational(new_numerator, denominator).to_s
-    mixed_fraction = "#{first_number.to_s} #{fraction}"
+    number = (numerator / denominator).floor
+    numerator = (numerator - (number * denominator))
+    fraction = Rational(numerator, denominator)
+    mixed_fraction = "#{number.to_s} #{fraction.to_s}"
   end
   mixed_fraction
 end
