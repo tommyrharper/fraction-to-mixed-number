234796781501b3ad0a7078415b3cf17dc0082ca2
tommyrharper <tommyrharper@gmail.com>
2020-07-24 13:06:13 +0100

    Add fix for test 3 - 2/3

--- lib/mixed_fraction.rb
+++ lib/mixed_fraction.rb
@@ -2,5 +2,6 @@ def mixed_fraction(s)
   n = s.split('/')[0].to_i
   d = s.split('/')[1].to_i
   num = (n / d).floor
-  (n % d).zero? ? num.to_s : "#{num} #{Rational(n - (num * d), d)}"
+  num1 = num.zero? ? nil : num.to_s + ' '
+  (n % d).zero? ? num.to_s : "#{num1}#{Rational(n - (num * d), d)}"
 end
