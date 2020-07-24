b9568470caa8642e6c6f99c04735b1973058b3e3
tommyrharper <tommyrharper@gmail.com>
2020-07-24 13:40:48 +0100

    Add solution for 10/-7

--- lib/mixed_fraction.rb
+++ lib/mixed_fraction.rb
@@ -2,14 +2,25 @@ def mixed_fraction(s)
   numerator = s.split('/')[0].to_i
   denom = s.split('/')[1].to_i
 
-  if numerator < 0 || denom < 0
+  if (numerator < 0 || denom < 0 ) && (numerator.abs <= denom.abs)
     return Rational(numerator, denom).to_s
+  elsif (numerator < 0 || denom < 0 )
+    puts "yo"
+    num = (numerator.to_f / denom.to_f).ceil
+    puts numerator
+    puts denom
+    puts numerator / denom
+    puts num
+    num_s = num.zero? ? nil : num.to_s + ' '
+    fraction = Rational((numerator - (num * denom)), denom.abs)
+
+  else
+
+    num = (numerator / denom).floor
+    num_s = num.zero? ? nil : num.to_s + ' '
+
+    fraction = Rational(numerator - (num * denom), denom)
   end
-
-  num = (numerator / denom).floor
-  num_s = num.zero? ? nil : num.to_s + ' '
-
-  fraction = Rational(numerator - (num * denom), denom)
 
   (numerator % denom).zero? ? num.to_s : "#{num_s}#{fraction}"
 end
