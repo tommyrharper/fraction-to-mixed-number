c559c005aa97c7c75eaf5dfb26da833b067ec0fe
tommyrharper <tommyrharper@gmail.com>
2020-07-24 14:00:25 +0100

    Add solution for test 10

--- lib/mixed_fraction.rb
+++ lib/mixed_fraction.rb
@@ -1,26 +1,17 @@
 def mixed_fraction(s)
   numerator = s.split('/')[0].to_i
   denom = s.split('/')[1].to_i
-
-  if ((numerator < 0) ^ (denom < 0 )) && (numerator.abs <= denom.abs)
+  if ((numerator < 0) ^ (denom < 0 )) && (numerator.abs < denom.abs)
+    puts "here"
     return Rational(numerator, denom).to_s
   elsif ((numerator < 0) ^ (denom < 0) )
-    puts "yo"
     num = (numerator.to_f / denom.to_f).ceil
-    puts numerator
-    puts denom
-    puts numerator / denom
-    puts num
     num_s = num.zero? ? nil : num.to_s + ' '
     fraction = Rational((numerator - (num * denom)).abs, denom.abs)
-
   else
-
     num = (numerator / denom).floor
     num_s = num.zero? ? nil : num.to_s + ' '
-
     fraction = Rational(numerator - (num * denom), denom)
   end
-
   (numerator % denom).zero? ? num.to_s : "#{num_s}#{fraction}"
 end
