ec711e757db91787026f763cbe8dd1d70a246c1e
tommyrharper <tommyrharper@gmail.com>
2020-07-24 12:35:21 +0100

    Add solution to test 2

--- lib/mixed_fraction.rb
+++ lib/mixed_fraction.rb
@@ -1,3 +1,16 @@
 def mixed_fraction(simple_fraction)
-  '4 2/3'
+
+  numerator = simple_fraction.split('/')[0].to_i
+  denominator = simple_fraction.split('/')[1].to_i
+  remainder = numerator % denominator
+
+  if remainder == 0
+    mixed_fraction = (numerator / denominator).to_s
+  else 
+    first_number = (numerator / denominator).floor
+    new_numerator = (numerator - (first_number * denominator)).to_s
+    fraction = Rational(new_numerator, denominator).to_s
+    mixed_fraction = "#{first_number.to_s} #{fraction}"
+  end
+  mixed_fraction
 end
