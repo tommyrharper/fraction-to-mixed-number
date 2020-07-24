5a05a10c65915f5ba2786658470d1b2e328cc849
tommyrharper <tommyrharper@gmail.com>
2020-07-24 13:08:57 +0100

    Change variable name from num1 to num_s

--- lib/mixed_fraction.rb
+++ lib/mixed_fraction.rb
@@ -2,6 +2,6 @@ def mixed_fraction(s)
   n = s.split('/')[0].to_i
   d = s.split('/')[1].to_i
   num = (n / d).floor
-  num1 = num.zero? ? nil : num.to_s + ' '
-  (n % d).zero? ? num.to_s : "#{num1}#{Rational(n - (num * d), d)}"
+  num_s = num.zero? ? nil : num.to_s + ' '
+  (n % d).zero? ? num.to_s : "#{num_s}#{Rational(n - (num * d), d)}"
 end
