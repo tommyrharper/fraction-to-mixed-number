8fd50ade4e53fa754646ffdb2a2c0a4a20d5f606
tommyrharper <tommyrharper@gmail.com>
2020-07-24 13:21:10 +0100

    Cut out put statements

--- lib/mixed_fraction.rb
+++ lib/mixed_fraction.rb
@@ -1,10 +1,6 @@
 def mixed_fraction(s)
   numerator = s.split('/')[0].to_i
-  puts 'numerator'
-  puts numerator
   denom = s.split('/')[1].to_i
-  puts 'denom'
-  puts denom
 
   if numerator < 0
     return Rational(numerator, denom).to_s
@@ -12,14 +8,8 @@ def mixed_fraction(s)
 
   num = (numerator / denom).floor
   num_s = num.zero? ? nil : num.to_s + ' '
-  puts 'num'
-  puts num
-  puts 'num_s'
-  puts num_s
 
   fraction = Rational(numerator - (num * denom), denom)
-  puts 'fraction'
-  puts fraction
 
   (numerator % denom).zero? ? num.to_s : "#{num_s}#{fraction}"
 end
