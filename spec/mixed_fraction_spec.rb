e28c5d2a09077ed307ef4b0503b6344afac5f8c4
tommyrharper <tommyrharper@gmail.com>
2020-07-24 12:11:33 +0100

    Add first test

--- spec/mixed_fraction_spec.rb
+++ spec/mixed_fraction_spec.rb
@@ -1,1 +1,7 @@
-require '../lib/mixed_faction.rb'
+require './mixed_fraction.rb'
+
+describe 'mixed_fraction' do
+  it 'returns 4 2/3 for 42/9' do
+    expect(mixed_fraction('42/9')).to eq '4 2/3'
+  end
+end
