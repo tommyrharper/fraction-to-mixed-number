1b4e45beda268b1a205bc84b8a060c7f65f93d85
tommyrharper <tommyrharper@gmail.com>
2020-07-24 12:19:59 +0100

    Add second test

--- spec/mixed_fraction_spec.rb
+++ spec/mixed_fraction_spec.rb
@@ -4,4 +4,8 @@ describe 'mixed_fraction' do
   it 'returns 4 2/3 for 42/9' do
     expect(mixed_fraction('42/9')).to eq '4 2/3'
   end
+
+  it 'returns 2 for 6/3' do
+    expect(mixed_fraction('42/9')).to eq '4 2/3'
+  end
 end
